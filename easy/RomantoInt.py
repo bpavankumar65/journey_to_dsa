adb8a9575370e72701f331ad59ead2dd72f149eb
pabanda <pavan.bandaru@daimler.com>
2024-01-09 12:52:12 +0100

    easy and double linked

--- easy/RomantoInt.py
+++ easy/RomantoInt.py
@@ -19,4 +19,18 @@ def romanToInt(s):
             flag = False
     return sum
 
+# recommended
+
+def roman_to_int(s):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(s) - 2):
+        if roman_dict[s[i + 1]] > roman_dict[s[i]]:
+            result -= roman_dict[s[i]]
+        else:
+            result += roman_dict[s[i]]
+    return result + roman_dict[s[-1]]
+
+
 romanToInt("LMC")
+print(roman_to_int("MCMI"))
